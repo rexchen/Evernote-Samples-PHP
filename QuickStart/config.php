dfc05dc62783722f72e84c80f43847b9cec52b83
rexchen <animal1004@hotmail.com>
2013-10-05 02:21:12 +0800

    change config.php

--- QuickStart/config.php
+++ QuickStart/config.php
@@ -6,6 +6,6 @@ define('AUTH_PATH', '/Evernote-Samples-PHP/QuickStart/auth/');
 define('AUTH_SECURITY_SALT', 'LDFmiilYf8Fyw5W10rx4W1KsVrieQCnpBzzpTBWA5vJidQKDx8pMJbmw28R1C4n');
 
 //Evernote API
-define('EVERNOTE_CONSUMER_KEY', 'slidenote');
-define('EVERNOTE_CONSUMER_SECRET', '703858afc45adebd');
+define('EVERNOTE_CONSUMER_KEY', '');
+define('EVERNOTE_CONSUMER_SECRET', '');
 define('EVERNOTE_SANDBOX', FALSE);
